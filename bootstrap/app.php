e1c91c888fc2dcc538bf3411f4fa0aa7590b226f
İsmail Çimen <64399530+ismailcimencom@users.noreply.github.com>
2026-05-18 14:26:26 +0300

    fix: trusted proxies headers for Cloudflare SSL

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
         $middleware->appendToGroup('web', TrackOnlineStatus::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
